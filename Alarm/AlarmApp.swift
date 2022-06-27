37fb9623f6835d80030b14b425492ccdc74f4d55
kimdoyoung <57849386+coby5502@users.noreply.github.com>
2022-06-27 23:58:44 +0900

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by COBY_PRO on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
